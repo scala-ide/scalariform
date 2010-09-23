8f3ec0769a5c8904f3f2f5b343001ebc3d61b77c
Matt Russell <MattRussellUK@gmail.com>
2010-09-23 07:58:20 +0100

    Add 2.8.1-SNAPSHOT to build

--- scalariform/src/main/scala/scalariform/parser/ScalaParserException.scala
+++ scalariform/src/main/scala/scalariform/parser/ScalaParserException.scala
@@ -1,3 +1,3 @@
 package scalariform.parser
 
-class ScalaParserException(message: String) extends RuntimeException(message) {}
+class ScalaParserException(message: String) extends RuntimeException(message)
