7e6d884c504a6de7009ac4df8c137ed5fb111a04
jadamcrain <jadamcrain@gmail.com>
2011-01-08 11:59:22 -0500

    All scalariform formatting options now exposed as plugin parameters.

--- scalariform.maven-plugin/src/main/java/org/scalariform/ScalariformMojo.java
+++ scalariform.maven-plugin/src/main/java/org/scalariform/ScalariformMojo.java
@@ -22,11 +22,56 @@ public class ScalariformMojo extends AbstractMojo {
      * Base directory of the project
      *
      * @parameter expression="${basedir}"
+     * @required
      */
     protected String baseDir;
 
+    /**
+     *  @parameter default-value=false
+     */
+    protected boolean alignParameters;
+
+    /**
+     *  @parameter default-value=false
+     */
+    protected boolean compactStringConcatenation;
+
+    /**
+     *  @parameter default-value=true
+     */
+    protected boolean doubleIndentClassDeclaration;
+
+    /**
+     *  @parameter default-value=2
+     */
+    protected int indentSpaces;
+
+    /**
+     *  @parameter default-value=false
+     */
+    protected boolean preserveSpaceBeforeArguments;
+
+    /**
+     *  @parameter default-value=false
+     */
+    protected boolean rewriteArrowSymbols;
+
+    /**
+     *  @parameter default-value=false
+     */
+    protected boolean spaceBeforeColon;
+
+    
     public void execute() throws MojoExecutionException {
-	MojoFormatter.format(baseDir);
+
+	MojoFormatter.format(baseDir, 
+                             alignParameters, 
+                             compactStringConcatenation,
+                             doubleIndentClassDeclaration,
+                             preserveSpaceBeforeArguments,
+                             rewriteArrowSymbols,
+                             spaceBeforeColon,
+                             indentSpaces);
     }
 
 }
