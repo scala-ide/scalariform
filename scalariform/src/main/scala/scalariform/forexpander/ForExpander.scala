2e0a8418991b59a00ccdd6083e7ae37394f0d92d
Matt Russell <MattRussellUK@gmail.com>
2011-03-08 17:58:22 +0000

    wip

--- scalariform/src/main/scala/scalariform/forexpander/ForExpander.scala
+++ scalariform/src/main/scala/scalariform/forexpander/ForExpander.scala
@@ -11,6 +11,8 @@ case class Filter(test: Expr) extends NscEnumerator
 
 object ForExpander {
 
+  private val MaxTupleArity = 22
+
   def expandFor(forExpr: ForExpr): CallExpr = {
     val (mapName, flatMapName) = if (forExpr.yieldOption.isDefined) ("map", "flatMap") else ("foreach", "foreach")
     makeFor(mapName, flatMapName, makeEnumerators(forExpr.enumerators), forExpr.body)
@@ -55,6 +57,23 @@ object ForExpander {
         makeCombination(flatMapName, rhs, pat, Expr(List(makeFor(mapName, flatMapName, rest, body))))
       case ValFrom(pat, rhs) :: Filter(test) :: rest ⇒
         makeFor(mapName, flatMapName, ValFrom(pat, Expr(List(makeCombination("withFilter", rhs, pat, test)))) :: rest, body)
+      case ValFrom(pat , rhs) :: rest ⇒
+        val valeqs = rest.take(MaxTupleArity - 1).takeWhile(_.isInstanceOf[ValEq])
+        val rest1 = rest.drop(valeqs.length)
+        val pats = valeqs map { case ValEq(pat, _) ⇒ pat }
+        val rhss = valeqs map { case ValEq(_, rhs) ⇒ rhs }
+        
+        val defpat1 = makeBind(pat)
+        val defpats = pats map makeBind
+        val pdefs = (defpats, rhss).zipped flatMap makePatDef
+        val ids = (defpat1 :: defpats) map makeValue
+        val rhs1 = makeForYield(
+          List(ValFrom(pos, defpat1, rhs)),
+          Block(pdefs, atPos(wrappingPos(ids)) { makeTupleTerm(ids, true) }) setPos wrappingPos(pdefs))
+        val allpats = (pat :: pats) map (_.duplicate)
+        val vfrom1 = ValFrom(r2p(pos.startOrPoint, pos.point, rhs1.pos.endOrPoint), atPos(wrappingPos(allpats)) { makeTuple(allpats, false) }, rhs1)
+        makeFor(mapName, flatMapName, vfrom1 :: rest1, body)
+        //throw new UnsupportedOperationException
     }
 
   }
