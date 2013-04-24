7ed87b7f8aaa4048a0fec811ea8cd33665488507
Matt Russell <MattRussellUK@gmail.com>
2013-04-24 13:50:52 +0100

    Update versions for 0.1.4 release

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object ScalariformBuild extends Build {
 
   lazy val commonSettings = Defaults.defaultSettings ++ SbtScalariform.defaultScalariformSettings ++ Seq(
     organization := "org.scalariform",
-    version := "0.1.4-SNAPSHOT",
+    version := "0.1.4",
     scalaVersion := "2.10.0",
     crossScalaVersions := Seq(
       "2.10.0",
