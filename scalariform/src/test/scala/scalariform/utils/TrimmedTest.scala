6824978b08946485fc9b8c891e4da840d0127f40
Simon Schäfer <mail@antoras.de>
2015-12-28 16:01:43 +0100

    Remove warnings about automatic insertion of parens in test project

--- scalariform/src/test/scala/scalariform/utils/TrimmedTest.scala
+++ scalariform/src/test/scala/scalariform/utils/TrimmedTest.scala
@@ -8,12 +8,12 @@ class TrimmedTest extends FlatSpec with ShouldMatchers {
 
   it should "just work" in {
 
-    Trimmed.unapply("").get should equal ("", "", "")
-    Trimmed.unapply("foo").get should equal ("", "foo", "")
-    Trimmed.unapply("  foo").get should equal ("  ", "foo", "")
-    Trimmed.unapply("  foo  ").get should equal ("  ", "foo", "  ")
-    Trimmed.unapply("  foo bar  ").get should equal ("  ", "foo bar", "  ")
-    Trimmed.unapply(" ").get should equal (" ", "", "")
+    Trimmed.unapply("").get should equal (("", "", ""))
+    Trimmed.unapply("foo").get should equal (("", "foo", ""))
+    Trimmed.unapply("  foo").get should equal (("  ", "foo", ""))
+    Trimmed.unapply("  foo  ").get should equal (("  ", "foo", "  "))
+    Trimmed.unapply("  foo bar  ").get should equal (("  ", "foo bar", "  "))
+    Trimmed.unapply(" ").get should equal ((" ", "", ""))
 
   }
 
