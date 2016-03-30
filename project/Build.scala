5c09f1e3c8a55e8bf1c646f1d03804a573d18dbb
Marko Elezovic <marko@mentat-labs.com>
2016-03-30 02:18:35 +0200

    Refactored Build.scala Bumped SBT, Scala versions Bumped plugins & library dependencies GitHub is https by default Removed legacy Eclipse project settings

--- project/Build.scala
+++ project/Build.scala
@@ -6,63 +6,59 @@ import scalariform.formatter.preferences._
 import xerial.sbt.Sonatype._
 import xerial.sbt.Sonatype.SonatypeKeys._
 import sbtassembly.AssemblyPlugin.autoImport._
+import sbtbuildinfo.BuildInfoPlugin.autoImport._
 
 object ScalariformBuild extends Build {
 
   lazy val commonSettings = inConfig(Test)(Defaults.testSettings) ++ SbtScalariform.defaultScalariformSettings ++ sonatypeSettings ++ Seq(
     organization := "org.scalariform",
-    profileName := "org.scalariform",
+    sonatypeProfileName := organization.value,
     version := "0.2.0-SNAPSHOT",
-    scalaVersion := "2.11.7",
+    scalaVersion := crossScalaVersions.value.head,
     crossScalaVersions := Seq(
-      "2.11.7",
+      "2.11.8",
       "2.10.6"
     ),
     exportJars := true, // Needed for cli oneJar
-    scalacOptions ++= (scalaBinaryVersion.value match {
-      case "2.11" => Seq(
-        "-deprecation:false",
-        "-encoding", "UTF-8",
-        "-feature",
-        "-language:_",
-        "-unchecked",
-        "-Xlint",
-        "-Xfuture",
-        "-Xfatal-warnings",
-        "-Yno-adapted-args",
-        "-Ywarn-dead-code",
-        "-Ywarn-unused-import",
-        "-Ywarn-unused"
-      )
-      case _ => Seq()
-    }),
-    credentials += Credentials(Path.userHome / ".m2" / "credentials")
+    scalacOptions ++= Seq(
+      "-deprecation:false",
+      "-encoding", "UTF-8",
+      "-feature",
+      "-language:_",
+      "-unchecked",
+      "-Xlint",
+      "-Xfuture",
+      "-Xfatal-warnings",
+      "-Yno-adapted-args",
+      "-Ywarn-dead-code",
+      "-Ywarn-unused-import",
+      "-Ywarn-unused"
+    ),
+    credentials ++= {
+      val creds = Path.userHome / ".m2" / "credentials"
+      if (creds.exists) Seq(Credentials(creds)) else Nil
+    }
   )
 
-  lazy val subprojectSettings = commonSettings ++ Seq(
-    ScalariformKeys.preferences <<= baseDirectory.apply(getScalariformPreferences))
+  lazy val subprojectSettings = commonSettings :+ (
+    ScalariformKeys.preferences := scalariformPreferences.value)
 
-  def getScalariformPreferences(dir: File) =
-    PreferencesImporterExporter.loadPreferences((dir / ".." / "formatterPreferences.properties").getPath)
-
-  lazy val root: Project = Project("root", file("."), settings = commonSettings) aggregate (scalariform, cli)
-
-  implicit class Regex(sc: StringContext) {
-    def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
+  def scalariformPreferences = Def.setting {
+    PreferencesImporterExporter.loadPreferences((baseDirectory.value / ".." / "formatterPreferences.properties").getPath)
   }
 
-  def getScalaTestDependency(scalaVersion: String) = scalaVersion match {
-    case r"2.11.\d+[-\w]*" | r"2.10.\d+[-\w]*" => "org.scalatest" %%  "scalatest" % "2.2.4" % "test"
-    case "2.9.3" => "org.scalatest" %% "scalatest" % "1.9.1" % "test"
-    case _       => "org.scalatest" %% "scalatest" % "1.7.2" % "test"
-  }
+  lazy val root = project in file(".") settings(commonSettings) aggregate(scalariform, cli)
 
-  def get2_11Dependencies(scalaVersion: String): List[ModuleID] = scalaVersion match {
-    case r"2.11.\d+[-\w]*" => List(
-      "org.scala-lang.modules" %% "scala-xml" % "1.0.1",
-      "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.1"
-    )
-    case _ => Nil
+  def getScalaTestDependency = "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+
+  def get2_11Dependencies = Def.setting {
+    CrossVersion.partialVersion(scalaVersion.value) match {
+      case Some((2, 11)) => Seq(
+        "org.scala-lang.modules" %% "scala-xml" % "1.0.5",
+        "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
+      )
+      case _ => Nil
+    }
   }
 
   def publishSettings(projectName: String) = Seq(
@@ -72,48 +68,45 @@ object ScalariformBuild extends Build {
     publishArtifact in (Compile, packageDoc) := true,
     publishArtifact in (Compile, packageSrc) := true,
     pomIncludeRepository := { _ ⇒ false },
-    sbtbuildinfo.Plugin.buildInfoKeys := Seq[sbtbuildinfo.Plugin.BuildInfoKey](version),
-    sourceGenerators in Compile <+= sbtbuildinfo.Plugin.buildInfo,
-    sbtbuildinfo.Plugin.buildInfoPackage := projectName
+    buildInfoKeys := Seq[BuildInfoKey](version),
+    buildInfoPackage := projectName,
+    publishTo := getPublishToRepo.value
   )
 
-  lazy val scalariform: Project = Project("scalariform", file("scalariform"), settings =
-    subprojectSettings ++ sbtbuildinfo.Plugin.buildInfoSettings ++ publishSettings("scalariform") ++
+  lazy val scalariform = project enablePlugins(sbtbuildinfo.BuildInfoPlugin) settings(
+    subprojectSettings ++ publishSettings("scalariform") ++
       Seq(
-        libraryDependencies <<= (scalaVersion, libraryDependencies) { (sv, deps) ⇒
-          deps ++ get2_11Dependencies(sv) :+ getScalaTestDependency(sv)
-        },
-        // sbt doesn't automatically load the content of the MANIFST.MF file, therefore we have to do it here by ourselves
+        libraryDependencies ++= get2_11Dependencies.value :+ getScalaTestDependency,
+        // sbt doesn't automatically load the content of the MANIFEST.MF file, therefore we have to do it here by ourselves
         packageOptions in Compile in packageBin += {
-          val m = Using.fileInputStream(new java.io.File("scalariform/META-INF/MANIFEST.MF"))(in => new java.util.jar.Manifest(in))
+          val m = Using.fileInputStream(file("scalariform/META-INF/MANIFEST.MF"))(in => new java.util.jar.Manifest(in))
           Package.JarManifest(m)
         },
-        testOptions in Test += Tests.Argument("-oI"),
-        publishTo <<= isSnapshot(getPublishToRepo)))
+        testOptions in Test += Tests.Argument("-oI")))
 
-  def getPublishToRepo(isSnapshot: Boolean) =
-    if (isSnapshot)
-      Some("snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
+  def getPublishToRepo = Def.setting {
+    if (isSnapshot.value)
+      Some(Opts.resolver.sonatypeSnapshots)
     else
-      Some("releases" at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
+      Some(Opts.resolver.sonatypeStaging)
+  }
 
-  lazy val cli = Project("cli", file("cli"), settings = subprojectSettings ++ publishSettings("cli") ++
-    sbtbuildinfo.Plugin.buildInfoSettings ++
+  lazy val cli = project enablePlugins(sbtbuildinfo.BuildInfoPlugin) settings(
+    subprojectSettings ++ publishSettings("cli") ++
     Seq(
       libraryDependencies += "commons-io" % "commons-io" % "1.4",
       mainClass in (Compile, packageBin) := Some("scalariform.commandline.Main"),
       mainClass in assembly := Some("scalariform.commandline.Main"),
-      publishTo <<= isSnapshot(getPublishToRepo),
       artifact in (Compile, assembly) := {
         val art = (artifact in (Compile, assembly)).value
         art.copy(`classifier` = Some("assembly"))
       }
     ) ++ addArtifact(artifact in (Compile, assembly), assembly)
-  ) dependsOn (scalariform)
+  ) dependsOn(scalariform)
 
   def pomExtraXml =
     <inceptionYear>2010</inceptionYear>
-    <url>http://github.com/scala-ide/scalariform</url>
+    <url>https://github.com/scala-ide/scalariform</url>
     <licenses>
       <license>
         <name>MIT License</name>
